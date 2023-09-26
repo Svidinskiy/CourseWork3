5d632dfd6046be26aab7343d21b5988c797d6f1b
Svidinskiy <100801664+Svidinskiy@users.noreply.github.com>
2023-09-26 22:41:17 +0400

    Сделана курсовая работа 3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Напишите пример перехвата и обработки исключения в блоке try-catch-метода.
+        try {
+            int result = divide(4, 0);
+            System.out.println("Результат: " + result);
+        }
+        catch (ArithmeticException e){
+            System.out.println("Произошла ошибка: " + e.getMessage());
+        }
+        finally {
+            System.out.println("Программа завершена.");
+        }
+
+    }
+
+    public static int divide(int x, int y){
+        if(y == 0){
+            throw new ArithmeticException("Деление на ноль невозможно");
+        }
+
+        return x/y;
     }
 }
